5cd372e7a3c6a6ed0fa438ee74b3e95fcd73c830
KannanMahamuni <56910385+KannanMahamuni@users.noreply.github.com>
2026-04-12 01:40:02 +0530

    Create tests/test_login.py

---
+++ tests/test_login.py
@@ -0,0 +1,9 @@
+"""
+Playwright test for login page that captures a screenshot after successful login.
+
+This file will be populated incrementally.
+"""
+
+from playwright.sync_api import Page
+
+# TODO: Implement test_login_and_screenshot
